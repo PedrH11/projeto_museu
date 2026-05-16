15e5f3f2332c909acf90e6affed46647e3c432ba
Miguel Totti <migueltotti2005@gmail.com>
2026-05-16 00:21:46 -0300

    feat: add featured artworks section

--- next_museu/src/app/page.tsx
+++ next_museu/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { RotatingArtworks } from '@/components/shared/landing/rotating-artworks';
+import { FeaturedArtworks } from '@/components/shared/landing/featured-artworks';
 import { HighlightCarousel } from '../components/shared/landing/carrossel';
 import { Faqs } from '../components/shared/landing/faq';
 import { Footer } from '../components/shared/landing/footer';
@@ -6,12 +7,15 @@ import { HeroMuseum } from '../components/shared/landing/hero';
 import { Navbar } from '../components/shared/landing/nav-bar';
 
 export default function Home() {
+  const hasHighlightedArtworks = true;
+
   return (
     <div className="relative flex min-h-screen flex-col">
       <Navbar />
       <main className="flex-grow flex flex-col items-center w-full overflow-x-hidden">
         <HeroMuseum />
         <HighlightCarousel />
+        {hasHighlightedArtworks && <FeaturedArtworks />}
         <RotatingArtworks />
         {/* <SearchSection /> */}
         {/* <Events /> */}
